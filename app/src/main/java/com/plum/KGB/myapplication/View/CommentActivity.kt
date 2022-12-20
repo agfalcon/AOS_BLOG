32556d7b7706707a691941de68be34d315b1ffb2
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2022-12-20 23:07:51 +0900

    2022.12.20 마무리

--- app/src/main/java/com/plum/KGB/myapplication/View/CommentActivity.kt
+++ app/src/main/java/com/plum/KGB/myapplication/View/CommentActivity.kt
@@ -2,17 +2,27 @@ package com.plum.KGB.myapplication.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.plum.KGB.myapplication.Adapter.EssayAdapter
+import com.plum.KGB.myapplication.ViewModel.CommentViewModel
 import com.plum.KGB.myapplication.databinding.ActivityCommentBinding
 
 class CommentActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityCommentBinding
+    lateinit var model: CommentViewModel
+    private var editID: Int = 0
+    var commentNum: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCommentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        model = ViewModelProvider(this)[CommentViewModel::class.java]
+        commentNum = intent.getIntExtra(EditShowActivity.COMMENT_NUM, 0)
+        editID = intent.getIntExtra(EssayAdapter.EDIT_ID, 0)
+        model.requestComment(editID)
     }
 }
--- app/src/main/java/com/plum/KGB/myapplication/ViewModel/CommentViewModel.kt
+++ app/src/main/java/com/plum/KGB/myapplication/ViewModel/CommentViewModel.kt
@@ -1,16 +1,71 @@
 package com.plum.KGB.myapplication.ViewModel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley.newRequestQueue
+import org.json.JSONArray
+import org.json.JSONObject
 
 class CommentViewModel(application: Application) : AndroidViewModel (application) {
     data class Comment(var id: Int, var editId: Int, var content: String, var date: String)
+
+    companion object{
+        const val SERVER_URL = "https://sg-blog-db-lzrnx.run.goorm.io/comment"
+        const val QUEUE_TAG = "CommentQueueTag"
+    }
 
     private val list = ArrayList<Comment>()
     private val _comments = MutableLiveData<ArrayList<Comment>>()
     val comments : LiveData<ArrayList<Comment>>
         get() = _comments
-    
+
+    private var queue: RequestQueue
+
+    init{
+        _comments.value = list
+        queue = newRequestQueue(getApplication())
+    }
+
+    fun requestComment(editId: Int){
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            "$SERVER_URL/$editId",
+            null,
+            {
+                //Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_LONG).show()
+                list.clear()
+                parseJson(it)
+                _comments.value = list
+                Toast.makeText(getApplication(), list.toString(), Toast.LENGTH_LONG).show()
+
+            },
+            {
+                Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_LONG).show()
+            }
+        )
+        request.tag = QUEUE_TAG
+        queue.add(request)
+    }
+
+    private fun parseJson(items: JSONArray){
+        for(i in 0 until items.length()){
+            val item = items[i] as JSONObject
+            val id = item.getInt("id")
+            val editId = item.getInt("edit_id")
+            val date = item.getString("date")
+            val content = item.getString("content")
+            list.add(Comment(id, editId, content, date))
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue.cancelAll(QUEUE_TAG)
+    }
 }
