775f87791e68995a56387d338a417faf0e157ff2
agfalcon <agfalcon12@gmail.com>
2022-12-20 20:04:26 +0900

    블로그 글 리스트 출력!!

--- app/src/main/java/com/plum/KGB/myapplication/MainActivity.kt
+++ app/src/main/java/com/plum/KGB/myapplication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.plum.KGB.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.plum.KGB.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var essayViewModel: EssayViewModel
+    private lateinit var essayAdapter : EssayAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        essayViewModel = ViewModelProvider(this)[EssayViewModel::class.java]
+        essayAdapter = EssayAdapter(essayViewModel)
+
+        essayViewModel.essays.observe(this){
+            essayAdapter.notifyDataSetChanged()
+        }
+
+        binding.listItem.apply{
+            layoutManager = LinearLayoutManager(application)
+            itemAnimator = DefaultItemAnimator()
+            setHasFixedSize(true)
+            adapter = essayAdapter
+        }
+        essayViewModel.requestEssay()
     }
 }
